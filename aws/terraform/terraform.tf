62b820b91cc1ac975ba827a2c29c0c7cc40498d4
Adam Cubel <adam.c.cubel@saic.com>
2024-10-01 20:50:11 +0000

    commenting out S3 state file

--- aws/terraform/terraform.tf
+++ aws/terraform/terraform.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "TF STATE BUCKET NAME"
-    key    = "FOLDER/FILE NAME"
-    region = "us-east-1"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket = "TF STATE BUCKET NAME"
+#     key    = "FOLDER/FILE NAME"
+#     region = "us-east-1"
+#   }
+# }
