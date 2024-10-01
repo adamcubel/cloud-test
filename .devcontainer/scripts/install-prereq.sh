794e6351ee3488a39fa20d6b31a170a0cb24393d
Adam Cubel <adam.c.cubel@saic.com>
2024-10-01 14:53:22 -0500

    removing userspace scripts from prereq

--- .devcontainer/scripts/install-prereq.sh
+++ .devcontainer/scripts/install-prereq.sh
@@ -30,12 +30,3 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-echo >> /root/.bashrc
-echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /root/.bashrc
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-source /root/.bashrc
-
-# Install k9s
-brew install derailed/k9s/k9s
